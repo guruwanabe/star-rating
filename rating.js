410af159b2b33968fa0354ca0b0889320921c8f3
Bogdan Stanciu <bp.stanciu@gmail.com>
2015-04-21 18:20:08 +0300

    refactor
    refactor

--- rating.js
+++ rating.js
@@ -54,7 +54,7 @@
         sendData:function() {
             $('#button-review').on('click', function() {
                 $.ajax({
-                    url: 'http://guruwanabe.github.io/guruwanabe/star-rating/star',
+                    url: 'http://guruwanabe.github.io/star-rating/star',
                     type: 'post',
                     dataType: 'json',
                     data: 'text=' + encodeURIComponent($('textarea[name=\'text\']').val()) + '&rating=' + encodeURIComponent($('input[name=\'rating\']').val() ? $('input[name=\'rating\']').val() : ''),
