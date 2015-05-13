4ce2f1e51c92cfda7648a0db1d70aa30c8da4c33
Bogdan Stanciu <bp.stanciu@gmail.com>
2015-05-13 09:56:12 +0300

    updates

--- rating.js
+++ rating.js
@@ -5,7 +5,7 @@
  */
 
 /* ========================================================================
- * JS needed for browser compatibility
+ * JS to do all the magic
  * ======================================================================== */
 
 ( function( $ ) {
@@ -14,80 +14,86 @@
 
 		ready: function() {
 			app.rating();
-            app.toolTips();
-            app.sendData();
+      app.toolTips();
+      app.sendData();
 		},
-        rating: function() {
-            $('.star').on('click hover', function(){
-                //ID is set to hidden input so we can send the value
-                var checkedValue = $(this).attr('id');
-                
-                if(!($(this).is(':checked') || $(this).hasClass('checked'))){
-                    $(this).addClass('checked') && $(this).nextAll().addClass('checked');
-                    $(this).parent().find('input[name=rating]').prop('checked', true).val(checkedValue);
-                }else{
-                    $(this).prevAll().removeClass('checked');
-                    $(this).parent().find('input[name=rating]').prop('checked', false).val(checkedValue);
-                }
+    rating: function() {
+        $('.star').on('click hover', function(){
+            //ID is set to hidden input so we can send the value
+            var checkedValue = $(this).attr('id');
+            
+            if(!($(this).is(':checked') || $(this).hasClass('checked'))){
+                $(this).addClass('checked') && $(this).nextAll().addClass('checked');
+                $(this).parent().find('input[name=rating]').prop('checked', true).val(checkedValue);
+            }else{
+                $(this).prevAll().removeClass('checked');
+                $(this).parent().find('input[name=rating]').prop('checked', false).val(checkedValue);
+            }
 
+        });
+        $('.star').hover(function(){
+            if(!$(this).hasClass('checked')){
+                $(this).addClass('checked') && $(this).nextAll().addClass('checked');
+            }else{
+                $(this).prevAll().removeClass('checked');
+            }
+
+        });
+    },
+    toolTips: function(){
+        var tooltips = $('[data-toggle="tooltip"]'),
+            placement = $(this).data('placement');
+            options = {
+              'trigger': 'hover focus',
+              'html': true,
+              'container': 'body',
+              'placement': placement
+            };
+        tooltips.tooltip(options)
+    },
+    sendData:function() {
+        //here your backend url
+        var ratingUrl = 'http://www.e-tutungerie.ro/index.php?route=product/product/write&product_id=182';
+
+        $('#button-review').on('click', function() {
+            $.ajax({
+                type: 'POST',
+                url: ratingUrl,
+                dataType: 'json',
+                data: 'text=' + encodeURIComponent($('textarea[name=\'text\']').val()) + '&rating=' + encodeURIComponent($('input[name=\'rating\']').val() ? $('input[name=\'rating\']').val() : ''),
+                    beforeSend: function() {
+                      $('.alert-warning, .alert-success').remove();
+                      $('#button-review').attr('disabled', true);
+                      $('.form-group').first().before('<div class="alert alert-info"><i class="fa fa-spinner fa-pulse"></i> Please wait!</div>');
+                    },
+                    complete: function() {
+                      $('#button-review').attr('disabled', false);
+                      $('.alert-info').remove();   
+                    },
+                    success: function(data) {
+                      if (data['error']) {
+                        //console
+                        $('#console').html(data['error']);
+                         //error message
+                        $('.form-group').first().before('<div class="alert alert-warning">' + data['error'] + '</div>');
+
+                        alert('Oups, we got an error');
+                      }
+                      if (data['success']) {
+                        //console
+                        $('#console').html(data['success']);
+                        //success message
+                        $('.form-group').first().before('<div class="alert alert-success">' + data['success'] + '</div>');
+                        $('input[name=\'name\']').val('');
+                        $('textarea[name=\'text\']').val('');
+                        $('input[name=\'rating\']:checked').attr('checked', '');
+
+                        alert('Yey, you rated your first post/product or whatever');
+                      }
+                    }
             });
-            $('.star').hover(function(){
-                if(!$(this).hasClass('checked')){
-                    $(this).addClass('checked') && $(this).nextAll().addClass('checked');
-                }else{
-                    $(this).prevAll().removeClass('checked');
-                }
-
-            });
-        },
-        toolTips: function(){
-            var tooltips = $('[data-toggle="tooltip"]'),
-                placement = $(this).data('placement');
-                options = {
-                    'trigger': 'hover focus',
-                    'html': true,
-                    'container': 'body',
-                    'placement': placement
-                };
-            tooltips.tooltip(options)
-        },
-        sendData:function() {
-            $('#button-review').on('click', function() {
-                $.ajax({
-                    url: 'http://guruwanabe.github.io/star-rating/star',
-                    type: 'post',
-                    dataType: 'json',
-                    data: 'text=' + encodeURIComponent($('textarea[name=\'text\']').val()) + '&rating=' + encodeURIComponent($('input[name=\'rating\']').val() ? $('input[name=\'rating\']').val() : ''),
-                        beforeSend: function() {
-                            $('.alert-warning, .alert-success').remove();
-                            $('#button-review').attr('disabled', true);
-                            $('.form-group').first().before('<div class="alert alert-info"><i class="fa fa-spinner fa-pulse"></i> Please wait!</div>');
-                        },
-                        complete: function() {
-                            $('#button-review').attr('disabled', false);
-                            $('.alert-info').remove();   
-                        },
-                        success: function(data) {
-                            if (data['error']) {
-                                $('#console').html(data['error']);
-                                $('.form-group').first().before('<div class="alert alert-warning">' + data['error'] + '</div>');
-
-                                alert('Oups, we got an error');
-                            }
-                            if (data['success']) {
-                                //success message
-                                $('#console').html(data['success']);
-                                $('.form-group').first().before('<div class="alert alert-success">' + data['success'] + '</div>');
-                                $('input[name=\'name\']').val('');
-                                $('textarea[name=\'text\']').val('');
-                                $('input[name=\'rating\']:checked').attr('checked', '');
-
-                                alert('Yey, you rated your first post/product or whatever');
-                            }
-                        }
-                });
-            });
-        }   
+        });
+    }   
 	};
 
 	$(window).on('ready', app.ready);
